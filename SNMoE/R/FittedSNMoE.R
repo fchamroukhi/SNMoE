88cc5e2eb9c8f738aab51e8ca454c07d21b64e03
Florian Lecocq <florian.lecocq@outlook.com>
2019-04-30 16:48:53 +0200

    Improve plots

--- SNMoE/R/FittedSNMoE.R
+++ SNMoE/R/FittedSNMoE.R
@@ -7,18 +7,43 @@ FittedSNMoE <- setRefClass(
   ),
   methods = list(
     plot = function() {
-      plot.default(modelSNMoE$X, modelSNMoE$Y, pch = "+", ylab = "y", xlab = "x")
+
+      plot.default(modelSNMoE$X, modelSNMoE$Y, ylab = "y", xlab = "x", cex = 0.7, pch = 3)
+      title(main = "Estimated mean and experts")
       for (k in 1:modelSNMoE$K) {
-        lines(modelSNMoE$X, statSNMoE$Ey_k[, k], lty = "dotted", col = "red")
+        lines(modelSNMoE$X, statSNMoE$Ey_k[, k], col = "red", lty = "dotted", lwd = 1.5)
       }
-      lines(modelSNMoE$X, statSNMoE$Ey, lwd = 2, col = "red")
+      lines(modelSNMoE$X, statSNMoE$Ey, col = "red", lwd = 1.5)
 
 
-      couleur = rainbow(modelSNMoE$K)
-      plot.default(modelSNMoE$X, statSNMoE$piik[, 1], type = "l", ylab = "Mixing probabilities", xlab = "x", col = couleur[1])
+      colorsvec = rainbow(modelSNMoE$K)
+      plot.default(modelSNMoE$X, statSNMoE$piik[, 1], type = "l", xlab = "x", ylab = "Mixing probabilities", col = colorsvec[1])
+      title(main = "Mixing probabilities")
       for (k in 2:modelSNMoE$K) {
-        lines(modelSNMoE$X, statSNMoE$piik[, k], col = couleur[k])
+        lines(modelSNMoE$X, statSNMoE$piik[, k], col = colorsvec[k])
       }
+
+      # Data, Estimated mean functions and 2*sigma confidence regions
+      plot.default(modelSNMoE$X, modelSNMoE$Y, ylab = "y", xlab = "x", cex = 0.7, pch = 3)
+      title(main = "Estimated mean and confidence regions")
+      lines(modelSNMoE$X, statSNMoE$Ey, col = "red", lwd = 1.5)
+      lines(modelSNMoE$X, statSNMoE$Ey - 2 * sqrt(statSNMoE$Vary), col = "red", lty = "dotted", lwd = 1.5)
+      lines(modelSNMoE$X, statSNMoE$Ey + 2 * sqrt(statSNMoE$Vary), col = "red", lty = "dotted", lwd = 1.5)
+
+      # Obtained partition
+      plot.default(modelSNMoE$X, modelSNMoE$Y, ylab = "y", xlab = "x", cex = 0.7, pch = 3)
+      title(main = "Estimated experts and clusters")
+      for (k in 1:modelSNMoE$K) {
+        lines(modelSNMoE$X, statSNMoE$Ey_k[, k], col = colorsvec[k], lty = "dotted", lwd = 1.5)
+      }
+      for (k in 1:modelSNMoE$K) {
+        index <- statSNMoE$klas == k
+        points(modelSNMoE$X[, index], modelSNMoE$Y[index, ], col = colorsvec[k], cex = 0.7, pch = 3)
+      }
+
+      # Observed data log-likelihood
+      plot.default(unlist(statSNMoE$stored_loglik), type = "l", col = "blue", xlab = "EM iteration number", ylab = "Observed data log-likelihood")
+      title(main = "Log-Likelihood")
 
     }
   )
